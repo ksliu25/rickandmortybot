c85bfb98cb6a02b5787f2c0c14218da453197606
Kenneth Liu <ksliu25@gmail.com>
2015-08-23 13:32:06 -0500

    initial

---
+++ bot_display.py
@@ -0,0 +1,10 @@
+import praw
+user_agent = "ksliu25 testrobo 0.1"
+r = praw.Reddit(user_agent = user_agent)
+subreddit = r.get_subreddit("pythonforengineers")
+
+for submission in subreddit.get_hot(limit = 5):
+    print "Title: ", submission.title
+    print "Text: ", submission.selftext
+    print "Score: ", submission.score
+    print "---------------------------------\n"
